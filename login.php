c1a3cd29fe1a13c6723ab26618dcda3a0739ca25
Denis Luciano <denis.lopes3012@gmail.com>
2018-11-14 16:18:52 -0200

    Lucas, o login não esta pronto, tem que termianr

--- login.php
+++ login.php
@@ -6,21 +6,16 @@ include_once "conexao.php";
 
 //Um teste se estamos recebendo pelo metodo post
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	echo "METODO POST";
-	//testando javascript dentro php
-	echo ("<script>alert('teste');</script>");
 
-	$name = $_POST["name"];
+
 	$email =$_POST["email"];
 	$senha = $_POST["senha"];
-	$verSenha = $_POST["senha-confirmar"];
-	$tipo = $_POST["tipo-cadastro"];
 
-	echo $name;
+
 	echo $email;
 	echo $senha;
-	echo $verSenha;
-	echo $tipo;
+
+
 	echo("<br><br><br><br>");
 
 	if($tipo == "estudante"){
