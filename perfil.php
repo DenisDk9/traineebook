40f2d0381d508620d52a87a037be646c4d888fff
Nvrmr <invictusnevermore@gmail.com>
2018-11-23 19:29:27 -0200

    Add files via upload

--- perfil.php
+++ perfil.php
@@ -160,7 +160,20 @@ $telefone=$result["telefone"];
         }
         else if($secao=="resumo"){
 
-            
+            echo '<form  >
+                <div class="form-group row">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">Nome: </label>
+                    <div class="col-sm-10">
+                    <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="'.$nome[0].'">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">Email: </label>
+                    <div class="col-sm-10">
+                    <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="'.$email.'">
+                    </div>
+                </div>
+             </form>';
 
 
         }
